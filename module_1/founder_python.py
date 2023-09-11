94026650b01499673cecc0efab8a163353ec1fa3
Omiko Emmanuel Ndubuisi <144232718+omiko-milzy1@users.noreply.github.com>
2023-09-11 14:34:56 +0100

    (6th commit) founder_python.py

---
+++ module_1/founder_python.py
@@ -0,0 +1,3 @@
+"""
+There are not many languages whose authors are known by name. Python was created by Guido van Rossum, born in 1956 in Haarlem, the Netherlands. Of course, Guido van Rossum did not develop and evolve all the Python components himself.
+"""
